888c63dbe85c2b13508fdc2350f6c5cd920ddede
Daemoonn <563141614@qq.com>
2017-10-22 15:14:23 +0800

    remove form post on index.htm to anthoer page

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,7 +16,7 @@ public class MainController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public ModelAndView hello(Model model){
         ModelAndView modelAndView = new ModelAndView("index");
 
@@ -23,6 +26,17 @@ public class MainController {
         List<User> userList = userDao.findAll();
         model.addAttribute("userList", userList);
 
+        User user = new User();
+        user.setId(123);
+        user.setUsername("Johnyy");
+        user.setPassword("123454132");
+        model.addAttribute("user", user);
+
         return modelAndView;
+    }
+
+    @PostMapping("/hello")
+    public String update(@ModelAttribute User user) {
+        return "result";
     }
 }
