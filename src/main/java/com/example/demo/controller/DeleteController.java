ada370cfc509d3ca3e89a05a854cb6a7832d26c6
Daemoonn <563141614@qq.com>
2017-10-25 16:18:20 +0800

    modify screenshots

--- src/main/java/com/example/demo/controller/DeleteController.java
+++ src/main/java/com/example/demo/controller/DeleteController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.dao.UserDao;
+import com.example.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
 
 @RestController
 public class DeleteController {
@@ -11,6 +14,15 @@ public class DeleteController {
 
     @RequestMapping(value = "/hello/{id}", method = RequestMethod.DELETE)
     public Object deleteUser(@PathVariable("id") Integer id) {
+        String path = "D:\\NIIThn\\demo\\src\\main\\webapp\\";
+        User userInDb = userDao.findOne(id);
+        try {
+            File DelFile = new File(path + userInDb.getAvataruuid() + ".jpg");
+            DelFile.delete();
+        } catch (Exception e) {
+            System.out.println("fromDeleteController------------------------error!");
+            System.out.println(e);
+        }
         userDao.delete(id);
         System.out.println(id + " was deleted----------------!!!!!!!!!!!!");
         return null;
